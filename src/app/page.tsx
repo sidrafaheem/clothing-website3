cb2478982a385cce93709049f0b4294e1ccfa271
sidrafaheem <sidrafaheem75@gmail.com>
2024-11-09 22:24:52 +0500

    Add files via upload
    direct upload without command line

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Hero from "@/components/Hero";
+import NewArrival from "@/components/NewArrival";
+
+
+
+export default function Home() {
+  return (
+<div>
+  <Hero/>
+  
+<NewArrival />
+</div>
+  );
+}
