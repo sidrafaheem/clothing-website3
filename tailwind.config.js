3c3999433f24d82ff9b7a6c96fa1d79387ca56dc
khoulafaheem <khoulafaheem@gmail.com>
2024-11-10 21:47:34 +0500

    frist-commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
